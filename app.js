2a298773c1721e076a40ff62124a9bf1b78ec243
ramsha275 <33019590+ramsha275@users.noreply.github.com>
2020-06-07 18:08:22 +0500

    Working upto Math expressions

---
+++ app.js
@@ -0,0 +1,227 @@
+// ASSIGNMENT 1 
+
+
+
+// Q1
+//alert("Hello Visitor !")
+
+// Q2
+//window.alert("Error! Please enter a valid password")
+
+// Q3
+//window.alert("Welcome to JS land...\nHappy Coding")
+
+// Q4
+// window.alert("Welcome to JS land...")
+// window.alert("Happy Coding")
+
+// Q5
+//console.log("Hello! I can run JS through my web browser's console")
+
+// 6 // 7
+ 
+
+// ASSIGNMENT 2 
+
+
+// Q1
+//var username;
+
+// Q2
+//var myName = "Ramsha Azeemi";
+
+// Q3
+// var message;
+// message="Hello World";
+// alert(message)
+
+// Q4
+// var name = "Sarah Ahmed";
+// var age = 21;
+// var course = "Web and Mobile App Developement" ;
+// alert(name) 
+// alert(age) 
+// alert(course) 
+
+// Q5
+// my_string = "PIZZA\nPIZZ\nPIZ\nPI\nP"
+// alert(my_string)
+
+// Q6
+// var email = "xyz@gmail.com" ;
+// alert("My email address is "+email) ;
+
+// Q7
+// var book;
+// book =  'A smarter way to learn JavaScript';
+// alert("I am trying to learn from the book "+book)
+
+// Q8
+//document.write("Yah! I can write HTML content through javaScript")
+
+// Q9 
+// var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// alert(design)
+// document.write(design)
+
+
+// ASSIGNMENT 3
+
+// Q1
+// var age;
+// age = 10;
+// alert("I am "+age+" years old")
+
+// Q2
+// var count = 3;
+// alert("You have visited this site "+count+" times")
+
+
+// Q3
+// var birth_year;
+// birth_year = 2005;
+// document.write("My birth year is " ,birth_year);
+// document.write("<br>Data type of my declared variable is number");
+
+
+// Q4
+// var site = "XYZ Clothing Store" 
+// var name = 'John Doe';
+// var pro_title = "T-Shirt" ;
+// var quantity = 5 ;
+
+// document.write("<strong>" ,name, "</strong> ordered <strong>" , quantity , " ", pro_title , "</strong>(s) on ",site)
+
+
+// ASSIGNMENT 4
+
+//Q1
+// var a , b , c;
+
+//Q2
+// Legal ------->      _name , name1 , $name_2 , f_name, name 
+// Illegal -------->    1age , #age , my age , my-age , @age
+
+//Q3
+// document.write(" <h3> Rules for naming JS Variables </h3> <br>")
+// document.write("a) Variable names can only contain numbers, $ and _.For example $my_1stVariable <br>")
+// document.write("b) Variables must begin with a letter ,$ or _. For example $name, _name or name <br>")
+// document.write("c) Variable names are case sensitive <br>")
+// document.write("d) Variable names should not be JS keyword <br>")
+
+
+// ASSIGNMENT 5
+
+//Q1
+
+// var num1 = 2 , num2 = 8 , sum;
+// sum = num1+num2;
+// document.write("Sum of ",num1," and ",num2," is ",sum)
+
+//Q2
+// var sub , mul , div , mod , num1 = 2 , num2 = 8 ;
+// sub = num2 - num1;
+// mul = num1 * num2 ;
+// div = num2 / num1;
+// mod = num2 % num1 ;
+
+// document.write("After subtraction of ",num1," and ",num2," the answer is ",sub, "<br>");
+// document.write("After multiplication of ",num1," and ",num2," the answer is ",mul, "<br>");
+// document.write("After division of ",num1," and ",num2," the answer is ",div, "<br>");
+// document.write("The modulus of ",num1," and ",num2," is ",mod, "<br>");
+
+// Q3
+// var a ;
+// document.write("Value after variable declaration is ",a, "<br>");
+// a = 5 ;
+// document.write("Initial value: ",a, "<br>");
+// a++ ;
+// document.write("Value after increment is: ",a, "<br>");
+// a = a + 7;
+// document.write("Value after addition is: ",a, "<br>");
+// a-- ;
+// document.write("Value after decrement is: ",a, "<br>");
+// document.write("The remainder is: ",a%3);
+
+// Q4
+// var ticket = 600;
+// document.write("Total cost to buy 5 tickets to a movie is ",ticket*5,"PKR")
+
+// Q5
+// var num = 2 ;
+// document.write("<h2>Table of ",num,"</h2>")
+// for(var i = 1 ; i<=10 ; i++)
+// {
+//     ans = num * i;
+//     document.write(num," * " ,i, " = ",ans, "<br>");
+// }
+
+// Q6
+
+// var c = 25 , f; 
+// var f = (c * (9/5))+32;
+// document.write(c ,"<sup>o</sup>C is ",f,"<sup>o</sup>F <br>");
+// var f = 70 , c;
+// var c = (f -32) * 5/9;
+// document.write(f ,"<sup>o</sup>F is ",c,"<sup>o</sup>C <br>");
+
+// Q7
+// var item1 = 650 , item2 = 100 , quan1 = 3 , quan2 = 7 , shipping_charges = 100;
+// var total = (item1*quan1) + (item2*quan2) + shipping_charges;
+// document.write("<h2> Shopping Cart </h2>");
+// document.write("Price of item 1 is ",item1,"<br>");
+// document.write("Quantity of item 1 is ",quan1,"<br>"); 
+// document.write("Price of item 2 is ",item2,"<br>");
+// document.write("Quantity of item 2 is ",quan2,"<br>");
+// document.write("Shipping Charges ",shipping_charges,"<br><br>");
+// document.write("Total cost of your order is ",total);
+
+// Q8
+// var total = 980 , obtained = 804 , per;
+// per = obtained*100/total ;
+// document.write("<h2> Mark Sheet </h2>")
+// document.write("Total Marks: ",total,"<br>");
+// document.write("Obtained Marks: ",obtained,"<br>");
+// document.write("Percentage: ",per,"%");
+
+//Q9 
+// var $ = 10 , riyals = 25 , pkr ;
+// pkr = $*104.80 + riyals*28; 
+// document.write("<h2> Currency in PKR </h2>");
+// document.write("Total currency in PKR: ",pkr);
+
+// Q10
+// var  num = 2;
+// exp = ((num + 5) * 10) / 2;
+// document.write("Answer: ",exp); 
+
+// Q11
+// var cur = 2020 , birth = 2000 ; 
+// var age = cur - birth ;
+// document.write("<h1> Age Calculator </h1>");
+// document.write("Current Year: ",cur,"<br>");
+// document.write("Birth Year: ",birth,"<br>");
+// document.write("Your age is: ",age,"<br>");
+
+// Q12
+// var r = 20;
+// const pie = 3.142;
+// var circum = 2*pie*r;
+// var area = pie *r*r;
+// document.write("<h1> The Geometrizer </h1>");
+// document.write("Radius of a circle: ",r,"<br>");
+// document.write("Circumference of a circle: ",circum,"<br>");
+// document.write(" Area of a circle: ",area); 
+
+// Q13
+// var snacks = "Lays" , age = 15 , max_age = 65 ,per_day = 3 , total ;
+// total = (max_age-age)*per_day ;
+// document.write("<h1>The Lifetime Supply Calculator </h1>");
+// document.write("Current age: ",age,"<br>");
+// document.write("Estimated Maximum Age: ",max_age,"<br>");
+// document.write("Amount of snacks per day: ",per_day,"<br>");
+// document.write("You will need ",total," ",snacks," to last you until the ripe old age of ",max_age);
+
+// ASSIGNMENT 6
+
+// Q1
